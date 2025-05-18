cb292f7b96e894d9dc5dc2215a7cf5d8709afdc1
Waled <waledmohamed2004@gmail.com>
2025-05-18 15:30:22 +0300

    add react files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to the App!</h1>
+      <p>This is a simple React application.</p>
+      <p>To get started, edit <code>src/App.tsx</code> and save to reload.</p>
+      <a href="https://vitejs.dev" target="_blank">Vite Documentation</a>
+      <a href="https://reactjs.org" target="_blank">React Documentation</a>
+    </>
+  )
+}
+
+export default App
